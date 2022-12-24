b462062ae3ceb274f5d6c0b804977d1eab061aaf
deepak jethwa <deepakjethwa95@gmail.com>
2022-12-24 18:01:24 +0530

    Add existing file

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ else if(e.target.innerHTML =='C'){
 string = "";
 document.querySelector('input').value= string;
 }
+// else if(e.target.innerHTML =='<img src="https://cdn-icons-png.flaticon.com/512/1/1661.png"
+//    alt="buttonpng" height="40px"/>'){
+//     string = string.slice(0,-1);
+//     document.querySelector('input').value= string;
+//     }
+
+    else if(e.target.innerHTML =='?'){
+    string = string.slice(0,-1);
+    document.querySelector('input').value= string;
+    }
 else{
     console.log(e.target)
     string = string + e.target.innerHTML;
